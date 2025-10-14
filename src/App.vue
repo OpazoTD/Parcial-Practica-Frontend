4cc0ef8204cada9d0e3f44d3732752b5ef9484df
OpazoTD <opazotomas0sd@gmail.com>
2025-10-14 17:32:31 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <!-- insertamos clases tailwind para ver si funciona -->
+  <h1 class="text-2xl text-emerald-950">Tailwind</h1>
+  <p class="text-emerald-500">
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
